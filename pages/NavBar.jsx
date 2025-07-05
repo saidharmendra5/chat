47307177416c8293800aa511867352ebc7fd282d
sai <vv92343@gmail.com>
2025-07-05 14:45:15 +0530

    updated UI

--- pages/NavBar.jsx
+++ pages/NavBar.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
 import './NavBar.css'; // Import the CSS file
+import { UserDetails } from '../contex/IsLoggedIn';
 
 const NavBar = () => {
+  const {loggeduser} = useContext(UserDetails);
   return (
     <>
       <div className='nav-bar'>
+        
         <NavLink to="chat" className="nav-link">Chat</NavLink>
         <NavLink to="add" className="nav-link">Add Friends</NavLink>
         <NavLink to="about" className="nav-link">About</NavLink>
-        <NavLink to="profile" className="nav-link">Profile</NavLink>
+        
+        <NavLink to="profile" className="nav-link">{loggeduser.fullname}</NavLink>
 
       </div>
       <Outlet />
