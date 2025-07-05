b634674b4235acb27590da7cf80c035e61971710
sai <vv92343@gmail.com>
2025-07-05 13:38:03 +0530

    added logout func

--- pages/Profile.jsx
+++ pages/Profile.jsx
@@ -1,10 +1,29 @@
 import React, { useContext } from 'react';
 import { UserDetails } from '../contex/IsLoggedIn';
 import './Profile.css';
+import { useNavigate } from 'react-router-dom';
 
 const Profile = () => {
   const { loggeduser } = useContext(UserDetails);
+  const navigate = useNavigate();
 
+const Logout = async () => {
+  try {
+    const res = await fetch('http://localhost:8000/chat/logout', {
+      method: 'POST',
+      credentials: 'include', // Important to send cookies
+    });
+
+    if (res.ok) {
+      console.log('Logged out successfully');
+      navigate('/');
+    } else {
+      console.error('Logout failed');
+    }
+  } catch (err) {
+    console.error('Error during logout:', err);
+  }
+};
   return (
     <div className="profile-container">
       <h2 className="profile-title">Profile</h2>
@@ -22,6 +41,11 @@ const Profile = () => {
       <p className="profile-meta">
         Account created on <strong>{new Date(loggeduser.createdAt).toLocaleDateString()}</strong>
       </p>
+      <div className="logout-wrapper">
+  <button className="logout-button" onClick={Logout}>Logout</button>
+</div>
+
+
     </div>
   );
 };
